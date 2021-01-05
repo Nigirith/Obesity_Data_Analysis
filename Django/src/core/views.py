d80c8ac03a7072d52cf3f713c49d6ba8d49faa15
thomas.loutrel-roux <thomas.loutrel@edu.devinci.fr>
2021-01-05 17:02:29 +0100

    news

--- Django/src/core/views.py
+++ Django/src/core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def test_view(request):
-    data = {
-        'name': 'john',
-        'age': 23
-    }
-    return JsonResponse(data)
+import pandas as pd
+import json
+
+my_data = pd.read_csv("D://Thomas//Bureau//Python for Data Analysis//Obesity_Data_Analysis//ObesityDataSet_raw_and_data_sinthetic.csv")
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = json.loads(my_data.to_json())
+        return Response(data)
