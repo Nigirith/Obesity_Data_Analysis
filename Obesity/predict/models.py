7c1f58a3a237a775f7be4b8fdcfa2b8eb8249460
thomas.loutrel-roux <thomas.loutrel@edu.devinci.fr>
2021-01-08 22:38:08 +0100

    i sleep

--- Obesity/predict/models.py
+++ Obesity/predict/models.py
@@ -1,3 +1,30 @@
+
 from django.db import models
 
-# Create your models here.
+
+class PredResults(models.Model):
+
+    gender = models.FloatField()
+    age = models.FloatField()
+    height = models.FloatField()
+    weight = models.FloatField()
+    family_history_with_overweight = models.FloatField()
+    frequent_consumption_of_high_caloric_food = models.FloatField()
+    frequency_of_consumption_of_vegetables = models.FloatField()
+    number_of_main_meals = models.FloatField()
+    consumption_of_food_between_meals = models.FloatField()
+    smoke = models.FloatField()
+    consumption_of_water_daily = models.FloatField()
+    calories_consumption_monitoring = models.FloatField()
+    physical_activity_frequency = models.FloatField()
+    time_using_technology_devices = models.FloatField()
+    consumption_of_alcohol = models.FloatField()
+    MTRANS_Automobile = models.FloatField()
+    MTRANS_Bike = models.FloatField()
+    MTRANS_Motorbike = models.FloatField()
+    MTRANS_Public_Transportation = models.FloatField()
+    MTRANS_Walking = models.FloatField()
+    classification = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.classification
--- Obesity/predict/views.py
+++ Obesity/predict/views.py
@@ -11,24 +11,48 @@ def predict_chances(request):
     if request.POST.get('action') == 'post':
 
         # Receive data from client
-        sepal_length = float(request.POST.get('sepal_length'))
-        sepal_width = float(request.POST.get('sepal_width'))
-        petal_length = float(request.POST.get('petal_length'))
-        petal_width = float(request.POST.get('petal_width'))
+        gender = float(request.POST.get('gender'))
+        age = float(request.POST.get('age'))
+        height = float(request.POST.get('height'))
+        weight = float(request.POST.get('weight'))
+        family_history_with_overweight = float(request.POST.get('family_history_with_overweight'))
+        frequent_consumption_of_high_caloric_food = float(request.POST.get('frequent_consumption_of_high_caloric_food'))
+        frequency_of_consumption_of_vegetables = float(request.POST.get('frequency_of_consumption_of_vegetables'))
+        number_of_main_meals = float(request.POST.get('number_of_main_meals'))
+        consumption_of_food_between_meals = float(request.POST.get('consumption_of_food_between_meals'))
+        smoke = float(request.POST.get('smoke'))
+        consumption_of_water_daily = float(request.POST.get('consumption_of_water_daily'))
+        calories_consumption_monitoring = float(request.POST.get('calories_consumption_monitoring'))
+        physical_activity_frequency = float(request.POST.get('physical_activity_frequency'))
+        time_using_technology_devices = float(request.POST.get('time_using_technology_devices'))
+        consumption_of_alcohol = float(request.POST.get('consumption_of_alcohol'))
+        means_of_transport = float(request.POST.get('means_of_transport'))
+
+        MTRANS_Automobile, MTRANS_Bike, MTRANS_Motorbike, MTRANS_Public_Transportation, MTRANS_Walking = 0,0,0,0,0
+
+        if means_of_transport == 0:
+            MTRANS_Automobile = 1
+        elif means_of_transport == 1:
+            MTRANS_Bike = 1
+        elif means_of_transport == 2:
+            MTRANS_Motorbike = 1
+        elif means_of_transport == 3:
+            MTRANS_Public_Transportation = 1
+        else:
+            MTRANS_Walking = 1
+
+        MTRANS_Automobile, MTRANS_Bike, MTRANS_Motorbike, MTRANS_Public_Transportation, MTRANS_Walking = float(MTRANS_Automobile), float(MTRANS_Bike), float(MTRANS_Motorbike), float(MTRANS_Public_Transportation), float(MTRANS_Walking)
 
         # Unpickle model
-        model = pd.read_pickle(r"C:\Users\azander\Downloads\new_model.pickle")
+        model = pd.read_pickle("D:/Thomas/Bureau/Python for Data Analysis/Obesity_Data_Analysis/new_model.pickle")
         # Make prediction
-        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+        result = model.predict([[gender, age, height, weight,family_history_with_overweight, frequent_consumption_of_high_caloric_food, frequency_of_consumption_of_vegetables, number_of_main_meals,consumption_of_food_between_meals, smoke, consumption_of_water_daily, calories_consumption_monitoring,physical_activity_frequency, time_using_technology_devices, consumption_of_alcohol, MTRANS_Automobile, MTRANS_Bike, MTRANS_Motorbike, MTRANS_Public_Transportation, MTRANS_Walking]])
 
         classification = result[0]
 
-        PredResults.objects.create(sepal_length=sepal_length, sepal_width=sepal_width, petal_length=petal_length,
-                                   petal_width=petal_width, classification=classification)
+        PredResults.objects.create(gender=gender, age=age, height=height, weight=weight, family_history_with_overweight=family_history_with_overweight, frequent_consumption_of_high_caloric_food=frequent_consumption_of_high_caloric_food, frequency_of_consumption_of_vegetables=frequency_of_consumption_of_vegetables, number_of_main_meals=number_of_main_meals, consumption_of_food_between_meals=consumption_of_food_between_meals, smoke=smoke, consumption_of_water_daily=consumption_of_water_daily, calories_consumption_monitoring=calories_consumption_monitoring, physical_activity_frequency=physical_activity_frequency, time_using_technology_devices=time_using_technology_devices, consumption_of_alcohol=consumption_of_alcohol, MTRANS_Automobile=MTRANS_Automobile, MTRANS_Bike=MTRANS_Bike, MTRANS_Motorbike=MTRANS_Motorbike, MTRANS_Public_Transportation=MTRANS_Public_Transportation, MTRANS_Walking=MTRANS_Walking, classification=classification)
 
-        return JsonResponse({'result': classification, 'sepal_length': sepal_length,
-                             'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
-                            safe=False)
+        return JsonResponse({'result': classification, 'gender': gender, 'age': age, 'height': height, 'weight': weight, 'family_history_with_overweight': family_history_with_overweight, 'frequent_consumption_of_high_caloric_food': frequent_consumption_of_high_caloric_food, 'frequency_of_consumption_of_vegetables': frequency_of_consumption_of_vegetables, 'number_of_main_meals': number_of_main_meals, 'consumption_of_food_between_meals': consumption_of_food_between_meals, 'smoke': smoke, 'consumption_of_water_daily': consumption_of_water_daily, 'calories_consumption_monitoring': calories_consumption_monitoring, 'physical_activity_frequency': physical_activity_frequency, 'time_using_technology_devices': time_using_technology_devices, 'consumption_of_alcohol': consumption_of_alcohol, 'MTRANS_Automobile': MTRANS_Automobile, 'MTRANS_Bike': MTRANS_Bike, 'MTRANS_Motorbike': MTRANS_Motorbike, 'MTRANS_Public_Transportation': MTRANS_Public_Transportation, 'MTRANS_Walking': MTRANS_Walking}, safe=False)
 
 
 def view_results(request):
