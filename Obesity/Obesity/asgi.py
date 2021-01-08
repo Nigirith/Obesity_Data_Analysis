11093c3adc180081f1178b8d4d6df1b103cd287b
thomas.loutrel-roux <thomas.loutrel@edu.devinci.fr>
2021-01-08 17:41:57 +0100

    new

--- Obesity/Obesity/asgi.py
+++ Obesity/Obesity/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for obesity_api project.
+ASGI config for Obesity project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'obesity_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Obesity.settings')
 
 application = get_asgi_application()
